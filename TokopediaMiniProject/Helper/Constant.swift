3997580147a530ad49f1f80b66bb8c9e76e5b7ad
jan <jan@code.id>
2022-01-14 22:56:48 +0700

    add/change:
    1. create constant to define API product

---
+++ TokopediaMiniProject/Helper/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  TokopediaMiniProject
+//
+//  Created by Mac on 14/01/22.
+//
+
+import Foundation
+
+struct Constant {
+    static let APIProduct: String = "https://hades.tokopedia.com/category/v1/tree/all"
+}
