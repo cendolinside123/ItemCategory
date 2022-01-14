f61442d736919d28558d352d579451f4d390f7b9
jan <jan@code.id>
2022-01-14 22:44:52 +0700

    add/change:
    1. create struct SpecialStageUseCase and implement StageNetworkProvider on it, adding alamofire request for load data from tokped API

---
+++ TokopediaMiniProject/Controller/AllStage/StageUseCase/SpecialStageUseCase.swift
@@ -0,0 +1,39 @@
+//
+//  SpecialStageUseCase.swift
+//  TokopediaMiniProject
+//
+//  Created by Mac on 14/01/22.
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+
+struct SpecialStageUseCase {
+    
+}
+
+extension SpecialStageUseCase: StageNetworkProvider {
+    
+    
+    func fetchProduct(completion: @escaping (NetworkResult<[Product]>) -> Void) {
+        
+        AF.request("https://hades.tokopedia.com/category/v1/tree/all", method: .get, parameters: Optional<String>.none, encoder: URLEncodedFormParameterEncoder.default, headers: nil, interceptor: nil, requestModifier: nil).responseJSON(completionHandler:  { response in
+            switch response.result {
+            case .success(let data):
+                let getJSON = JSON(data)
+                
+                var getListProduct: [Product] = []
+                
+                for product in getJSON["categoryAllList"]["categories"].arrayValue {
+                    getListProduct.append(Product(json: product))
+                }
+                completion(.success(getListProduct))
+            case .failure(let error):
+                completion(.failed(error))
+            }
+        })
+    }
+    
+    
+}
