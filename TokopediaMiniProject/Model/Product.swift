045432c86ba7f16d001c3e643fa7742c84dfdaa4
jan <jan@code.id>
2022-01-14 22:02:29 +0700

    add/change:
    1. setup make model Product can decode type JSON value to Product type

--- TokopediaMiniProject/Model/Product.swift
+++ TokopediaMiniProject/Model/Product.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 struct Product {
     let id: String
@@ -17,4 +18,24 @@ struct Product {
     let applinks: String
     let iconBannerURL: String
     let child: [Product]
+}
+
+extension Product {
+    init(json: JSON) {
+        id = json["id"].stringValue
+        name = json["name"].stringValue
+        identifier = json["identifier"].stringValue
+        url = json["url"].stringValue
+        iconImageUrl = json["iconImageUrl"].stringValue
+        parentName = json["parentName"].stringValue
+        applinks = json["applinks"].stringValue
+        iconBannerURL = json["iconBannerURL"].stringValue
+        
+        var childProduct = [Product]()
+        for product in json["child"].arrayValue {
+            childProduct.append(Product(json: product))
+        }
+        child = childProduct
+        
+    }
 }
