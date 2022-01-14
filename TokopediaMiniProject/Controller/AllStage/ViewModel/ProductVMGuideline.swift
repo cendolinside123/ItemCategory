7d04578ed6069b0a6db89e618fa318cc3049fd04
jan <jan@code.id>
2022-01-14 23:12:32 +0700

    add:
    1. create protocol ProductVMGuideline with some method and property on it

--- TokopediaMiniProject/Controller/AllStage/ViewModel/ProductVMGuideline.swift
+++ TokopediaMiniProject/Controller/AllStage/ViewModel/ProductVMGuideline.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+protocol ProductVMGuideline {
+    func loadProduct(reloadTime: Int)
+    func searchProduct(keyword: String)
+    var productResult: (([Product]) -> Void)? { get set }
+    var filterResult: (([Product]) -> Void)? { get set }
+    var fetchError: ((String) -> Void)? { get set }
+    var result: [Product] { get set }
+}
