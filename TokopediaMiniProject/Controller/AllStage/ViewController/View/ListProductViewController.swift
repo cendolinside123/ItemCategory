7cce6683c964eaacacf8cb711c90843debcad826
jan <jan@code.id>
2022-01-16 10:27:09 +0700

    add/change:
    1. first create/setup ListProductViewController (re-setup UI later)

---
+++ TokopediaMiniProject/Controller/AllStage/ViewController/View/ListProductViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ListProductViewController.swift
+//  TokopediaMiniProject
+//
+//  Created by Mac on 16/01/22.
+//
+
+import UIKit
+
+class ListProductViewController: UIViewController {
+    
+    private let loadingSpinner = UIActivityIndicatorView()
+    private let loadingView = UIView()
+    private let tableContent = UITableView()
+    var typeViewController: VCType?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setLayout()
+        setConstraints()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    private func setLayout() {
+        view.backgroundColor = .white
+        setLoadingView()
+        setProductTable()
+    }
+    
+    private func setConstraints() {
+        let views: [String: Any] = ["loadingView": loadingView, "tableContent": tableContent, "loadingSpinner": loadingSpinner]
+        let metrix: [String: Any] = [:]
+        var constraints = [NSLayoutConstraint]()
+        
+        //MARK: tableContent and loadingView constraints
+        tableContent.translatesAutoresizingMaskIntoConstraints = false
+        loadingView.translatesAutoresizingMaskIntoConstraints = false
+        let hTableContent = "H:|-5-[tableContent]-5-|"
+        let vTableContentLoadingView = "V:|-[loadingView]-0-[tableContent]-|"
+        let hLoadingView = "H:|-0-[loadingView]-0-|"
+        
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: hTableContent, options: .alignAllTop, metrics: metrix, views: views)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: vTableContentLoadingView, options: .alignAllLeading, metrics: metrix, views: views)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: hLoadingView, options: .alignAllTop, metrics: metrix, views: views)
+        let loadingViewHeight = NSLayoutConstraint(item: loadingView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 1/9, constant: 0)
+        loadingViewHeight.identifier = "loadingViewHeight"
+        constraints += [loadingViewHeight]
+        
+        //MARK: loadingSpinner constraints
+        loadingSpinner.translatesAutoresizingMaskIntoConstraints = false
+        constraints += [NSLayoutConstraint(item: loadingSpinner, attribute: .centerX, relatedBy: .equal, toItem: loadingView, attribute: .centerX, multiplier: 1, constant: 0)]
+        constraints += [NSLayoutConstraint(item: loadingSpinner, attribute: .centerY, relatedBy: .equal, toItem: loadingView, attribute: .centerY, multiplier: 1, constant: 0)]
+        
+        NSLayoutConstraint.activate(constraints)
+        
+    }
+    
+    private func setLoadingView() {
+        loadingSpinner.color = .gray
+        loadingView.addSubview(loadingSpinner)
+        loadingSpinner.startAnimating()
+        loadingView.backgroundColor = .white
+        view.addSubview(loadingView)
+    }
+    
+    private func setProductTable() {
+        tableContent.backgroundColor = .white
+        view.addSubview(tableContent)
+    }
+    
+    private func setupTable() {
+        tableContent.delegate = self
+        tableContent.dataSource = self
+        tableContent.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        tableContent.tableFooterView = UIView()
+    }
+    
+}
+extension ListProductViewController {
+    func vcWorkType(type: VCType) {
+        self.typeViewController = type
+    }
+}
+extension ListProductViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+}
+
