9889ad6df4eee1cb7eacbdf4bb58d01d94437620
jan <jan@code.id>
2022-01-17 21:42:23 +0700

    add/change:
    1. create expandProduct(child:) on protocol ProductVMGuideline
    2. create hideAllProduct(child:) on protocol ProductVMGuideline
    3. create hideSpesificProduct(child:) on protocol ProductVMGuideline
    4. create closure toggleResult that receive input array Product and boolean on protocol ProductVMGuideline

--- TokopediaMiniProject/Controller/AllStage/ViewModel/ProductVMGuideline.swift
+++ TokopediaMiniProject/Controller/AllStage/ViewModel/ProductVMGuideline.swift
@@ -10,8 +10,12 @@ import Foundation
 protocol ProductVMGuideline {
     func loadProduct(reloadTime: Int)
     func searchProduct(keyword: String)
+    func expandProduct(child: [Product])
+    func hideAllProduct(id: String)
+    func hideSpesificProduct(child: [Product])
     var productResult: (([Product]) -> Void)? { get set }
     var filterResult: (([Product]) -> Void)? { get set }
+    var toggleResult: (([Product], Bool) -> Void)? { get set }
     var fetchError: ((Error) -> Void)? { get set }
     var result: [Product] { get set }
 }
