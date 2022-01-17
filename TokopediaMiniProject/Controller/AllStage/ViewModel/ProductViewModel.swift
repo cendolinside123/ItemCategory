b45efb308a63b6fde6295bb5aee999e7be9b93ef
jan <jan@code.id>
2022-01-17 23:40:50 +0700

    add/change: 1. add closure toggleResult (base from protocol ProductVMGuideline) 2. change filterProduct(keyWord:listProduct:) filter process 3. create populateAlSearch(listProduct:) 4. create process for hideSpesificProduct(child:) to hide low level product 5. create process expandProduct(child:) expand product child 6. create process hideAllProduct(id:) to hide all product child 7. change search product process on searchProduct(keyword:)

--- TokopediaMiniProject/Controller/AllStage/ViewModel/ProductViewModel.swift
+++ TokopediaMiniProject/Controller/AllStage/ViewModel/ProductViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 class ProductViewModel {
     var productResult: (([Product]) -> Void)?
     var filterResult: (([Product]) -> Void)?
+    var toggleResult: (([Int], Bool) -> Void)? //note: how to use, #1 param list of index 2# if false mean delete true mean adding
     var fetchError: ((Error) -> Void)?
     var result: [Product] = []
     private var tempProduct: [Product] = []
@@ -32,7 +33,24 @@ extension ProductViewModel {
             let getChild: [Product] = self.filterProduct(keyWord: keyWord, listProduct: product.child)
             
             if product.name.contains(keyWord) || getChild.count > 0 {
-                getResult.append(Product(id: product.id, name: product.name, identifier: product.identifier, url: product.url, iconImageUrl: product.iconImageUrl, parentName: product.parentName, applinks: product.applinks, iconBannerURL: product.iconBannerURL, child: getChild, level: product.level))
+                getResult.append(Product(id: product.id, name: product.name, identifier: product.identifier, url: product.url, iconImageUrl: product.iconImageUrl, parentName: product.parentName, applinks: product.applinks, iconBannerURL: product.iconBannerURL, child: getChild, level: product.level, root: product.root))
+            }
+            
+        }
+        
+        return getResult
+    }
+    
+    private func populateAlSearch(listProduct: [Product]) -> [Product]{
+        var getResult: [Product] = []
+        
+        for product in listProduct {
+            
+            let getChild: [Product] = populateAlSearch(listProduct: product.child)
+            
+            getResult.append(Product(id: product.id, name: product.name, identifier: product.identifier, url: product.url, iconImageUrl: product.iconImageUrl, parentName: product.parentName, applinks: product.applinks, iconBannerURL: product.iconBannerURL, child: product.child, level: product.level, root: product.root))
+            if getChild.count > 0 {
+                getResult += getChild
             }
             
         }
@@ -42,6 +60,59 @@ extension ProductViewModel {
 }
 
 extension ProductViewModel: ProductVMGuideline {
+    func hideSpesificProduct(child: [Product]) {
+        var listIndex: [Int] = []
+        
+        for getProduct in child {
+            if let getIndex = self.result.firstIndex(where: { $0 == getProduct }) {
+                listIndex.append(getIndex)
+            }
+        }
+        
+        for getProduct in child {
+            if let getIndex = self.result.firstIndex(where: { $0 == getProduct }) {
+                self.result.remove(at: getIndex)
+            }
+        }
+        self.toggleResult?(listIndex, false)
+    }
+    
+    func expandProduct(child: [Product]) {
+        if self.tempProduct.count == 0 {
+            self.tempProduct = self.result
+        }
+        
+        if let getIndex = self.result.firstIndex(where: {$0.child == child}), getIndex + 1 <= self.result.count {
+            self.result.insert(contentsOf: child, at: getIndex + 1)
+            
+            var listIndex: [Int] = []
+            
+            for index in 0...child.count - 1 {
+                listIndex.append((getIndex + 1) + index)
+            }
+            self.toggleResult?(listIndex, true)
+        }
+        
+    }
+    
+    func hideAllProduct(id: String) {
+        
+        let getAllRemovedChild = self.result.filter({
+            $0.root == id
+        }).count
+        
+        if let getIndex = self.result.firstIndex(where: {$0.root == id}) {
+            var listIndex: [Int] = []
+            for index in 0...getAllRemovedChild - 1 {
+                listIndex.append(getIndex + index)
+            }
+            self.result.removeAll(where: {
+                $0.root == id
+            })
+            self.toggleResult?(listIndex, false)
+        }
+    }
+    
     func loadProduct(reloadTime: Int) {
         useCase.fetchProduct(completion: { [weak self] response in
             switch response {
@@ -62,12 +133,19 @@ extension ProductViewModel: ProductVMGuideline {
         if keyword == "" {
             self.result = self.tempProduct
             self.tempProduct = []
-            self.filterResult?(self.result)
+//            self.filterResult?(self.result)
+            self.productResult?(self.result)
             return
         }
         
-        self.tempProduct = self.result
+        if self.tempProduct.count == 0 {
+            self.tempProduct = self.result
+        } else {
+            self.result = self.tempProduct
+        }
+        
         self.result = filterProduct(keyWord: keyword, listProduct: self.result)
+        self.result = populateAlSearch(listProduct: self.result)
         self.filterResult?(self.result)
     }
     
