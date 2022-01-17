c7c44e79d71191cc918d8b5af899f8dcf9d8f5fa
jan <jan@code.id>
2022-01-17 10:57:46 +0700

    change: 1. change UIView value of searchBar 2. create process on callback leftButonaction for pop view controller and searchText forget input text from search bar and set all callback into bind() function 3. change constraint setup for navBarStack (heigh and top) 4. fix wrong constraint name 5. set navBar left button text , title kabel, and border width

--- TokopediaMiniProject/Controller/AllStage/ViewController/View/StageTwoViewController.swift
+++ TokopediaMiniProject/Controller/AllStage/ViewController/View/StageTwoViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class StageTwoViewController: UIViewController {
 
     private let navBar = DefaultNavBar()
-    private let searchBar = UIView()
+    private let searchBar = SearchBar()
     private let emptySpaceNav = UIView()
     private let emptySpaceSearch = UIView()
     private let navBarStackView: UIStackView = {
@@ -34,6 +34,7 @@ class StageTwoViewController: UIViewController {
         // Do any additional setup after loading the view.
         addLayouts()
         addConstraints()
+        bind()
     }
     
 
@@ -46,6 +47,15 @@ class StageTwoViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    private func bind() {
+        searchBar.searchText = { [weak self] keyword in
+            self?.searchKeyword(keyword: keyword)
+        }
+        navBar.leftButtonAction = { [weak self] in
+            self?.navigationController?.popViewController(animated: true)
+        }
+    }
     
     private func addLayouts() {
         view.backgroundColor = .white
@@ -64,13 +74,13 @@ class StageTwoViewController: UIViewController {
         //MARK: navBarStackView and emptyContent constraints
         navBarStackView.translatesAutoresizingMaskIntoConstraints = false
         emptyContent.translatesAutoresizingMaskIntoConstraints = false
-        let vNavBarStackEmptyContent = "V:|-0-[navBarStackView]-1-[emptyContent]-0-|"
+        let vNavBarStackEmptyContent = "V:|-[navBarStackView]-1-[emptyContent]-0-|"
         let hNavBarStack = "H:|-0-[navBarStackView]-0-|"
         let hEmptyContent = "H:|-0-[emptyContent]-0-|"
         constraints += NSLayoutConstraint.constraints(withVisualFormat: vNavBarStackEmptyContent, options: .alignAllLeading, metrics: matrix, views: views)
         constraints += NSLayoutConstraint.constraints(withVisualFormat: hNavBarStack, options: .alignAllTop, metrics: matrix, views: views)
         constraints += NSLayoutConstraint.constraints(withVisualFormat: hEmptyContent, options: .alignAllTop, metrics: matrix, views: views)
-        constraints += [NSLayoutConstraint(item: navBarStackView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 3/9, constant: 0)]
+        constraints += [NSLayoutConstraint(item: navBarStackView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 1/9, constant: 0)]
         
         //MARK: navBar constraints
         navBar.translatesAutoresizingMaskIntoConstraints = false
@@ -82,7 +92,7 @@ class StageTwoViewController: UIViewController {
         //MARK: searchBar constraints
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         let hSearchBar = "H:|-0-[searchBar]-0-|"
-        let vSearchBar = "V:|-0-[navsearchBarBar]-0-|"
+        let vSearchBar = "V:|-0-[searchBar]-0-|"
         constraints += NSLayoutConstraint.constraints(withVisualFormat: vSearchBar, options: .alignAllLeading, metrics: matrix, views: views)
         constraints += NSLayoutConstraint.constraints(withVisualFormat: hSearchBar, options: .alignAllTop, metrics: matrix, views: views)
         
@@ -100,11 +110,15 @@ class StageTwoViewController: UIViewController {
     private func addNavBar() {
         view.addSubview(emptySpaceNav)
         emptySpaceNav.addSubview(navBar)
+        navBar.setLeftButtonText(text: "<-")
+        navBar.setBorderWidth(width: 0)
+        
     }
     
     private func addSearchBar() {
         view.addSubview(emptySpaceSearch)
         emptySpaceSearch.addSubview(searchBar)
+        searchBar.setBorderWidth(width: 0)
     }
     
     private func addProductView() {
@@ -120,6 +134,9 @@ class StageTwoViewController: UIViewController {
         navBarStackView.addArrangedSubview(emptySpaceNav)
         navBarStackView.addArrangedSubview(emptySpaceSearch)
         view.addSubview(navBarStackView)
+        navBarStackView.layer.borderWidth = 1
+        navBarStackView.layer.borderColor = UIColor.black.cgColor
+        navBar.setTitle(title: "Stage 2")
     }
     
 }
