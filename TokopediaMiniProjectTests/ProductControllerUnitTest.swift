cd456db2fb283ee9007752a21122be4c13bf1311
jan <jan@code.id>
2022-01-19 03:11:35 +0700

    add/change:
    1. create UT testWrapingSelectedRootDataIndependentV2()

--- TokopediaMiniProjectTests/ProductControllerUnitTest.swift
+++ TokopediaMiniProjectTests/ProductControllerUnitTest.swift
@@ -184,4 +184,45 @@ class ProductControllerUnitTest: XCTestCase {
         wait(for: [wrapExpectation], timeout: 1)
     }
     
+    func testWrapingSelectedRootDataIndependentV2() {
+        let useCase: StageNetworkProvider = SuccessDataUseCase()
+        viewModel = ProductViewModel(useCase: useCase)
+        let loadExpectation = expectation(description: "should return product data")
+        let expandExpectation = expectation(description: "should expand product data")
+        let wrapExpectation = expectation(description: "should wrap product data")
+        
+        var getExpand: [Int] = []
+        
+        viewModel?.productResult = { result in
+            XCTAssertGreaterThan(result.count, 0)
+            loadExpectation.fulfill()
+        }
+        
+        viewModel?.toggleResult = { listIndex, status in
+            if status {
+                XCTAssertTrue(status)
+                XCTAssertNotEqual(listIndex.count, 0)
+                getExpand = listIndex
+                expandExpectation.fulfill()
+            } else {
+                XCTAssertFalse(status)
+                XCTAssertEqual(listIndex, getExpand)
+                wrapExpectation.fulfill()
+            }
+        }
+        
+        viewModel?.loadProduct(reloadTime: 3)
+        wait(for: [loadExpectation], timeout: 1)
+        viewModel?.selectExpandProduct(product: Product(id: "0", name: "Pilihan Untukmu", identifier: "", url: "https://www.tokopedia.com/p", iconImageUrl: "https://ecs7.tokopedia.net/img/attachment/2020/3/19/77305583/77305583_1ef56963-5102-44b3-938a-8935e5aac773.png", parentName: "", applinks: "", iconBannerURL: "https://ecs7.tokopedia.net/img/attachment/2020/3/19/77305583/77305583_01009a39-c22b-4229-8e4c-327d0f51cf6b.png", child: [
+            Product(id: "2700", name: "Makanan Ringan", identifier: "makanan-minuman_makanan-ringan", url: "https://www.tokopedia.com/p/makanan-minuman/makanan-ringan", iconImageUrl: "https://ecs7.tokopedia.net/img/attachment/2020/3/19/82120085/82120085_d861608c-32b9-4c27-af4e-2348100a4f78.png", parentName: "Makanan & Minuman", applinks: "tokopedia://category/2700?categoryName=Makanan+Ringan", iconBannerURL: "", child: [], level: 1) ,
+            Product(id: "2149", name: "Perawatan Kaki & Tangan", identifier: "perawatan-tubuh_perawatan-kaki-tangan", url: "https://www.tokopedia.com/p/perawatan-tubuh/perawatan-kaki-tangan", iconImageUrl: "https://ecs7.tokopedia.net/img/attachment/2020/3/19/82120085/82120085_15b61fa7-bece-4c9a-8032-67fb48beab10.png", parentName: "Perawatan Tubuh", applinks: "tokopedia://category/2149?categoryName=Perawatan+Kaki+%26+Tangan", iconBannerURL: "", child: [], level: 1)
+        ], level: 0), type: .independentV2)
+        wait(for: [expandExpectation], timeout: 1)
+        viewModel?.selectExpandProduct(product: Product(id: "0", name: "Pilihan Untukmu", identifier: "", url: "https://www.tokopedia.com/p", iconImageUrl: "https://ecs7.tokopedia.net/img/attachment/2020/3/19/77305583/77305583_1ef56963-5102-44b3-938a-8935e5aac773.png", parentName: "", applinks: "", iconBannerURL: "https://ecs7.tokopedia.net/img/attachment/2020/3/19/77305583/77305583_01009a39-c22b-4229-8e4c-327d0f51cf6b.png", child: [
+            Product(id: "2700", name: "Makanan Ringan", identifier: "makanan-minuman_makanan-ringan", url: "https://www.tokopedia.com/p/makanan-minuman/makanan-ringan", iconImageUrl: "https://ecs7.tokopedia.net/img/attachment/2020/3/19/82120085/82120085_d861608c-32b9-4c27-af4e-2348100a4f78.png", parentName: "Makanan & Minuman", applinks: "tokopedia://category/2700?categoryName=Makanan+Ringan", iconBannerURL: "", child: [], level: 1) ,
+            Product(id: "2149", name: "Perawatan Kaki & Tangan", identifier: "perawatan-tubuh_perawatan-kaki-tangan", url: "https://www.tokopedia.com/p/perawatan-tubuh/perawatan-kaki-tangan", iconImageUrl: "https://ecs7.tokopedia.net/img/attachment/2020/3/19/82120085/82120085_15b61fa7-bece-4c9a-8032-67fb48beab10.png", parentName: "Perawatan Tubuh", applinks: "tokopedia://category/2149?categoryName=Perawatan+Kaki+%26+Tangan", iconBannerURL: "", child: [], level: 1)
+        ], level: 0), type: .independentV2)
+        wait(for: [wrapExpectation], timeout: 1)
+    }
+    
 }
