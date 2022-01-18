24f9a8d692ba9d7eb2416b9f419b448216767b55
jan <jan@code.id>
2022-01-18 18:41:24 +0700

    add:
    1. register ProductCollectionViewCell to project

---
+++ TokopediaMiniProject/CustomUI/ProductCollectionViewCell/ProductCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ProductCollectionViewCell.swift
+//  TokopediaMiniProject
+//
+//  Created by Mac on 18/01/22.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+}
